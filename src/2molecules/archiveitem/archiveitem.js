276160ac98cd1380d58331b9b5a3193d0072213b
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-12 16:57:50 +0200

    Isolate html into js components: Archiveitem . Tune html and animation css

---
+++ src/2molecules/archiveitem/archiveitem.js
@@ -0,0 +1,22 @@
+import img from './../../assets/img/demo-article-2.png';
+
+const Archiveitem = `<div class="archiveitem">
+    <a class="archiveitem__imagelink" href="#">
+        <img class="archiveitem__image" src="${img}" alt="Verta, hikea ja kyyn eleita - Reidarsille jai luu kateen" />
+    </a>
+    <div class="archiveitem__content">
+        <h3 class="archiveitem__title">
+            <a href="#">
+                Verta, hikea ja kyyn eleita - Reidarsille jai luu kateen
+            </a>
+        </h3>
+        <p class="archiveitem__text">
+            Viikon takainen 4-0 tappio Kiekkosille aiheutti Reidarsin valmennusjohdossa tahattoman paniikkinappulan painamisen, ja kevatkauden toiseen otteluun laitettiin uusiksi niin aloittava maalivahti, pakkiparit kuin hyokkayskentatkin. Eipa sita...
+        </p>
+        <a href="#" class="link">
+            Lue lisaa
+        </a>
+    </div>
+</div>`;
+
+export default Archiveitem;

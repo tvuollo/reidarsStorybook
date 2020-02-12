dcddec86d49ce9d9af1ed10be4975cf1def8c81a
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-12 15:42:31 +0200

    Add story for Videoitem

---
+++ src/2molecules/videoitem/videoitem.stories.js
@@ -0,0 +1,23 @@
+import copyCodeBlock from '@pickra/copy-code-block';
+import { cssOverrides } from './../../../.storybook/copyCodeBlockOverrides';
+import './../../main.scss';
+import img from './../../assets/img/demo-article-1.png';
+
+const VideoitemHtml = `<a href="#" class="videoitem">
+    <span class="videoitem__thumb" style="background-image:url(${img});">
+        <img class="videoitem__img" src="${img}" alt="OFFSIDE REIDARS: We will remain!" />
+        <span class="videoitem__runtime">1:12</span>
+    </span>
+    <span class="videoitem__title">
+        OFFSIDE REIDARS: We will remain!
+    </span>
+    <span class="videoitem__meta">
+        Reidars | 4 paivaa sitten
+    </span>
+    <span class="videoitem__text">
+        Beerleague hockeycore from Lapland! Digital single available on most music streaming services.
+    </span>
+</a>`;
+export const Videoitem = () => VideoitemHtml + copyCodeBlock(VideoitemHtml, cssOverrides);
+
+export default { title: 'Molecules/Videoitem' };

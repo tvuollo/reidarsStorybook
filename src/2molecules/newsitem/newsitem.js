788d1b29e9452bf591d0b171862a2ff9f83d93c0
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-12 17:01:09 +0200

    Isolate html into js components: Newsitem

---
+++ src/2molecules/newsitem/newsitem.js
@@ -0,0 +1,12 @@
+import img from './../../assets/img/demo-article-2.png';
+
+const NewsitemHtml = `<a href="#" class="newsitem">
+    <img class="newsitem__image" src="${img}" alt="Reidarsin preseason alkoi tappiolla kauden avauspelissa" />
+    <span class="newsitem__bg" style="background-image:url(${img})"></span>
+    <span class="newsitem__content">
+        <span class="newsitem__title">Reidarsin preseason alkoi tappiolla kauden avauspelissa</span>
+        <span class="newsitem__meta">23.9.2019  |  Otteluraportti</span>
+    </span>
+</a>`;
+
+export default NewsitemHtml;
--- src/2molecules/newsitem/newsitem.stories.js
+++ src/2molecules/newsitem/newsitem.stories.js
@@ -1,16 +1,8 @@
 import copyCodeBlock from '@pickra/copy-code-block';
 import { cssOverrides } from './../../../.storybook/copyCodeBlockOverrides';
 import './../../main.scss';
-import img from './../../assets/img/demo-article-2.png';
+import NewsitemHtml from './newsitem';
 
-const NewsitemHtml = `<a href="#" class="newsitem">
-    <img class="newsitem__image" src="${img}" alt="Reidarsin preseason alkoi tappiolla kauden avauspelissa" />
-    <span class="newsitem__bg" style="background-image:url(${img})"></span>
-    <span class="newsitem__content">
-        <span class="newsitem__title">Reidarsin preseason alkoi tappiolla kauden avauspelissa</span>
-        <span class="newsitem__meta">23.9.2019  |  Otteluraportti</span>
-    </span>
-</a>`;
 export const Newsitem = () => NewsitemHtml + copyCodeBlock(NewsitemHtml, cssOverrides);
 
 export default { title: 'Molecules/NewsItem' };

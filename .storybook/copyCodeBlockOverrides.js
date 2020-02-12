20d8ac5b203de9f128c7c59d4751d2d8c6dc232c
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-12 14:13:45 +0200

    Set up project (package.json, storybook setup, enabling sass, copyCodeBlock enabling)

---
+++ .storybook/copyCodeBlockOverrides.js
@@ -0,0 +1,12 @@
+export const cssOverrides = {
+    cssOverrides: `
+        .container {
+            margin-top: 30px;
+            padding: 0;
+        }
+        .displayCode {
+            padding: 1rem;
+        }
+        .copyButton {}
+    `
+};

55f878e7ac177be18851c14dc3bf2edf3d10724d
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-13 12:42:14 +0200

    ListTitle renaming

--- src/1atoms/listtitle/listtitle.js
+++ src/1atoms/listtitle/listtitle.js
@@ -1,5 +1,5 @@
-const GamesListtitleHtml = `<h2 class="listtitle">Pelit</h2>`;
-const NewsListtitleHtml = `<h2 class="listtitle">Uusimmat uutiset</h2>`;
-const VideosListtitleHtml = `<h2 class="listtitle">Reidars TV</h2>`;
+const GamesListTitleHtml = `<h2 class="listtitle">Pelit</h2>`;
+const NewsListTitleHtml = `<h2 class="listtitle">Uusimmat uutiset</h2>`;
+const VideosListTitleHtml = `<h2 class="listtitle">Reidars TV</h2>`;
 
-export { GamesListtitleHtml, NewsListtitleHtml, VideosListtitleHtml };
+export { GamesListTitleHtml, NewsListTitleHtml, VideosListTitleHtml };
--- src/1atoms/listtitle/listtitle.stories.js
+++ src/1atoms/listtitle/listtitle.stories.js
@@ -1,10 +1,10 @@
 import copyCodeBlock from '@pickra/copy-code-block';
 import { cssOverrides } from './../../../.storybook/copyCodeBlockOverrides';
 import './../../main.scss';
-import { GamesListtitleHtml, NewsListtitleHtml, VideosListtitleHtml } from './ListTitle';
+import { GamesListTitleHtml, NewsListTitleHtml, VideosListTitleHtml } from './ListTitle';
 
-export const GamesListTitle = () => GamesListtitleHtml + copyCodeBlock(GamesListtitleHtml, cssOverrides);
-export const NewsListTitle = () => NewsListtitleHtml + copyCodeBlock(NewsListtitleHtml, cssOverrides);
-export const VideosListTitle = () => VideosListtitleHtml + copyCodeBlock(VideosListtitleHtml, cssOverrides);
+export const GamesListTitle = () => GamesListTitleHtml + copyCodeBlock(GamesListTitleHtml, cssOverrides);
+export const NewsListTitle = () => NewsListTitleHtml + copyCodeBlock(NewsListTitleHtml, cssOverrides);
+export const VideosListTitle = () => VideosListTitleHtml + copyCodeBlock(VideosListTitleHtml, cssOverrides);
 
 export default { title: 'Atoms/ListTitle' };

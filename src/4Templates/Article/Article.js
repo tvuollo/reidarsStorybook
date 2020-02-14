31c8bc313b3a0d3ddce9441ff8960fd61bcacac9
Tuomas Vuollo <tuomas.vuollo@tietotalo.fi>
2020-02-14 15:58:13 +0200

    Template: Article, create two variations. Add missing png image from Atoms/EndLine

--- src/4Templates/Article/Article.js
+++ src/4Templates/Article/Article.js
@@ -1,7 +1,39 @@
 import { ArticleHeaderHtml } from './../../2Molecules/ArticleHeader/ArticleHeader';
+import { GameItemReportHtml } from './../../2Molecules/GameItem/GameItem';
+import { ArticleBodyHtml } from './../../1Atoms/ArticleBody/ArticleBody';
+import { ArticleCaptionedImageHtml } from './../../2Molecules/ArticleCaptionedImage/ArticleCaptionedImage';
+import { EndLineHtml } from './../../1Atoms/EndLine/EndLine';
 
-const ArticleHtml = `<article class="article">
-    ${ArticleHeaderHtml}
+const BaseArticleHtml = `<article class="article">
+    <div class="article__header">
+        ${ArticleHeaderHtml}
+    </div>
+    <div class="article__content">
+        ${ArticleBodyHtml}
+    </div>
+    <div class="article__image">
+        ${ArticleCaptionedImageHtml}
+    </div>
+    <div class="article__content">
+        ${ArticleBodyHtml}
+    </div>
+    ${EndLineHtml}
 </article>`;
 
-export { ArticleHtml };
+const GameArticleHtml = `<article class="article">
+    <div class="article__header">
+        ${ArticleHeaderHtml}
+    </div>
+    <div class="article__score">
+        ${GameItemReportHtml}
+    </div>
+    <div class="article__content">
+        ${ArticleBodyHtml}
+    </div>
+    <div class="article__image">
+        ${ArticleCaptionedImageHtml}
+    </div>
+    ${EndLineHtml}
+</article>`;
+
+export { BaseArticleHtml, GameArticleHtml };
--- src/4Templates/Article/Article.stories.js
+++ src/4Templates/Article/Article.stories.js
@@ -1,8 +1,9 @@
 import copyCodeBlock from '@pickra/copy-code-block';
 import { cssOverrides } from './../../../.storybook/copyCodeBlockOverrides';
 import './../../main.scss';
-import { ArticleHtml } from './Article';
+import { BaseArticleHtml, GameArticleHtml } from './Article';
 
-export const Article = () => ArticleHtml + copyCodeBlock(ArticleHtml, cssOverrides);
+export const BaseArticle = () => BaseArticleHtml + copyCodeBlock(BaseArticleHtml, cssOverrides);
+export const GameArticle = () => GameArticleHtml + copyCodeBlock(GameArticleHtml, cssOverrides);
 
 export default { title: 'Templates/Article' };
